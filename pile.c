ef61c5d32d146ed48efe0a8d81b14176006b995b
KyllianCamp <campagnekyllian@gmail.com>
2022-03-21 23:01:32 +0100

    Ajout case pour tester fonction sur les piles

--- pile.c
+++ pile.c
@@ -2,7 +2,8 @@
 
 void initPile( T_Pile * P)
 {
-    P->nbElts = 0;
+    P->nbElts = -1; //Nombre d'élément mis à -1 car permet de l'utiliser comme indice de la case du tableau (1er element en indice 0)
+                    //Plus simple de mettre -1 ici plutôt que d'ajouter -1 partout pour obtenir l'indice de la case voulu.
 }
 
 
@@ -19,7 +20,7 @@ int pilepleine(const  T_Pile *P)
 
 int pilevide(const  T_Pile *P)
 {
-    if (P->nbElts <= 0)
+    if (P->nbElts < 0)
     {
         return 1;
     }
@@ -30,10 +31,10 @@ int pilevide(const  T_Pile *P)
 
 int empiler( T_Pile *P, T_Elt e) //renvoie 0 si pile pleine, sinon 1
 {
-    if (pilepleine(&P) != 1)
-    {
-        P->Elts[P->nbElts] = e;
+    if (pilepleine(P) != 1)
+    {   
         P->nbElts++;
+        P->Elts[P->nbElts] = e;
         return 1;
     }
     return 0;
@@ -43,10 +44,11 @@ int empiler( T_Pile *P, T_Elt e) //renvoie 0 si pile pleine, sinon 1
 
 int depiler( T_Pile *P, T_Elt *pelt)  //renvoie 0 si pile vide, sinon 1
 {
-    if (pilevide(&P) != 1)
+    if (pilevide(P) != 1)
     {
-        pelt = P->Elts[P->nbElts];
+        pelt = &P->Elts[P->nbElts];
         P->nbElts--;
+        afficherElt(pelt);
         return 1;
     }
 return 0;
@@ -63,7 +65,7 @@ T_Elt sommet(const  T_Pile *P)
 
 int hauteur(const  T_Pile *P)
 {
-    return P->nbElts;
+    return P->nbElts+1;
 }
 
 
@@ -71,20 +73,26 @@ void afficherPile(  T_Pile *P)
 {
     T_Pile PTempo;
     initPile(&PTempo);
-    while (pilevide(&P) != 1)
+    if (pilevide(P)!=1)
     {
-        afficherElt(P->Elts[P->nbElts]);
-        PTempo.Elts[PTempo.nbElts] = P->Elts[P->nbElts];
-        PTempo.nbElts++;
-        P->nbElts--;
+        while (pilevide(P) != 1)
+        {
+            afficherElt(&P->Elts[P->nbElts]);
+            PTempo.Elts[PTempo.nbElts] = P->Elts[P->nbElts];
+            PTempo.nbElts++;
+            P->nbElts--;
+        }
+        while (pilevide(&PTempo) != 1)
+        {
+            P->Elts[P->nbElts] = PTempo.Elts[PTempo.nbElts];
+            PTempo.nbElts--;
+            P->nbElts++;
+        }
     }
-    while (pilevide(&PTempo) != 1)
+    else
     {
-        P->Elts[P->nbElts] = PTempo.Elts[PTempo.nbElts];
-        PTempo.nbElts--;
-        P->nbElts++;
+        printf("pile vide \n");
     }
-    
 }
 
 
