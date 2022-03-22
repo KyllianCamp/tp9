6e9acce1f4ea752cf991b3a75d38a3779c8c6553
Achrafe <achrafe0903@gmail.com>
2022-03-22 10:57:07 +0100

    file.c

--- file.c
+++ file.c
@@ -40,8 +40,8 @@ int ajouter(T_File *ptrF,T_Elt *ptrE) // si espace libre, ajout en queue
     {   
         if(ptrF->Queue == MAX)
         {
-            ptrF->Queue = 0;
-            affecterElt(&ptrF->Elts[-5],ptrE);
+            ptrF->Queue = 1;
+            affecterElt(&ptrF->Elts[0],ptrE);
             return 1;
         }
         else
@@ -138,7 +138,6 @@ void afficherFile(T_File *ptrF)
             {
                 afficherElt(&ptrF->Elts[i]);
             }
-             afficherElt(&ptrF->Elts[-5]);
             for(int j=0;j<ptrF->Queue;j++)
             {
                 afficherElt(&ptrF->Elts[j]);
